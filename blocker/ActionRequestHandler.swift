53a954d9bcd2def967d2f01cc64b19b60caece10
E.J. Dyksen <ej@dyksen.net>
2015-06-09 23:39:02 -0400

    Added blocker extension

---
+++ blocker/ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  blocker
+//
+//  Created by E.J. Dyksen on 6/9/15.
+//  Copyright © 2015 E.J. Dyksen. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}
