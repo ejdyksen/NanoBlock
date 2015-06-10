b7c3abfbac7faaf8451b45c3a0959d6cc22f6fdf
E.J. Dyksen <ej@dyksen.net>
2015-06-09 23:38:04 -0400

    Initial Commit

---
+++ NanoBlock/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NanoBlock
+//
+//  Created by E.J. Dyksen on 6/9/15.
+//  Copyright © 2015 E.J. Dyksen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NanoBlock/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NanoBlock
+//
+//  Created by E.J. Dyksen on 6/9/15.
+//  Copyright © 2015 E.J. Dyksen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
